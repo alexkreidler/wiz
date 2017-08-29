53a5a604d5820aea780114232e730b6f46de8274
Timothy Higinbottom <timhigins@gmail.com>
2017-08-29 10:19:54 -0400

    Update tests

--- cmd/wiz/wiz_test.go
+++ cmd/wiz/wiz_test.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
-
-func ExampleSalutations() {
-	fmt.Println("hello, and")
-	fmt.Println("goodbye")
-	// Output:
-	// hello, and
-	// goodbye
-}
 
 func TestMain(m *testing.M) {
 	// call flag.Parse() here if TestMain uses flags
